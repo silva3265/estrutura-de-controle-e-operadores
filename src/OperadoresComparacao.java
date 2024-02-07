0b682e0a64f4035e9fa6c1e810b5f6dfeab71160
silva3265 <silva3265@gmail.com>
2024-02-07 15:58:33 -0300

    Operadores de Comparação

---
+++ src/OperadoresComparacao.java
@@ -0,0 +1,17 @@
+
+public class OperadoresComparacao {
+	
+	public static void main(String[] args) {
+	    int pesoProdutos = 900;
+	    int limitePesoCaminhao = 1000;
+
+	    boolean cargaExcedida = pesoProdutos > limitePesoCaminhao; //false
+	    System.out.printf("Carga excedida: %b%n", cargaExcedida);
+
+	    // boolean cargaLiberada = pesoProdutos < limitePesoCaminhao;
+	    // boolean cargaLiberada = pesoProdutos <= limitePesoCaminhao;
+	    boolean cargaLiberada = limitePesoCaminhao >= pesoProdutos;
+	    System.out.printf("Carga liberada: %b%n", cargaLiberada);    
+	  }
+
+}
