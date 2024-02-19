b18402398a043e4ac29e36ba2a21746ec43a3b5e
silva3265 <silva3265@gmail.com>
2024-02-19 16:19:19 -0300

    Operador ternário

---
+++ src/CalcularImpostos.java
@@ -0,0 +1,25 @@
+
+public class CalcularImpostos {
+
+	public static void main(String[] args) {
+		char tipoNotaFiscal = 'P';
+		double totalFaturado = 1200.30;
+
+		/*
+		 * double valorImpostos;
+		 * 
+		 * if (tipoNotaFiscal == 'S') { valorImpostos = totalFaturado * 0.16; } else {
+		 * valorImpostos = totalFaturado * 0.35; }
+		 */
+
+		// double valorImpostos = tipoNotaFiscal == 'S' ? totalFaturado * 0.16 :
+		// totalFaturado * 0.35;
+
+		double taxaImpostos = tipoNotaFiscal == 'S' ? 0.16 : 0.35; // Operador Ternario
+		double valorImpostos = totalFaturado * taxaImpostos;
+
+		System.out.printf("Total faturado: R$%.2f%n", totalFaturado);
+		System.out.printf("Valor dos impostos: R$%.2f%n", valorImpostos);
+	}
+
+}
