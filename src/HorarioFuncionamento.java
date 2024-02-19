c7cd3d9dbec5fb5d35e50de6ab978222107106fa
silva3265 <silva3265@gmail.com>
2024-02-19 15:26:44 -0300

    Switch Expressions

--- src/HorarioFuncionamento.java
+++ src/HorarioFuncionamento.java
@@ -1,34 +1,51 @@
 import java.util.Scanner;
 
 public class HorarioFuncionamento {
-	
+
 	public static void main(String[] args) {
 	    Scanner entrada = new Scanner(System.in);
 
-	    System.out.print("Digite um dia da semana (ex: seg, ter, qua, etc): ");
+	    System.out.print("Dia da semana (ex: seg, ter, qua, etc): ");
 	    String diaSemana = entrada.nextLine();
+
+	    System.out.print("Mês: ");
+	    int mes = entrada.nextInt();
+
+	    System.out.printf("Horário de funcionamento: %s%n", switch (diaSemana) {
+	      case "seg" -> {
+	         if (mes == 12) {
+	           yield "08:00 às 16:00"; // yield - produzir (é como se fosse um retorno)
+	         }
+	         yield "Fechado";
+	      }
+	      case "ter", "qua", "qui", "sex" -> "08:00 às 18:00";
+	      case "sab", "dom" -> "08:00 às 12:00";
+	      default -> "Dia inválido";
+	    });
+
+	    /*
+	    String horarioFuncionamento = switch (diaSemana) {
+	      case "seg" -> "Fechado";
+	      case "ter", "qua", "qui", "sex" -> "08:00 às 18:00";
+	      case "sab", "dom" -> "08:00 às 12:00";
+	      default -> "Dia inválido";
+	    };
+	    */
+
+	    /*
 	    String horarioFuncionamento;
 
 	    switch (diaSemana) {
-	      case "seg":
-	        horarioFuncionamento = "Fechado";
-	        break;
-	      case "ter":
-	      case "qua":
-	      case "qui":
-	      case "sex":
-	        horarioFuncionamento = "08:00 às 18:00";
-	        break;
-	      case "sab":
-	      case "dom":
+	      case "seg" -> horarioFuncionamento = "Fechado";
+	      case "ter", "qua", "qui", "sex" -> horarioFuncionamento = "08:00 às 18:00";
+	      case "sab", "dom" -> {
 	        horarioFuncionamento = "08:00 às 12:00";
-	        break;
-	      default:
-	        horarioFuncionamento = "Dia inválido";
+	      }
+	      default -> horarioFuncionamento = "Dia inválido";
 	    }
+	    */
 
-	    System.out.printf("Horário de funcionamento: %s%n", horarioFuncionamento);
+	    // System.out.printf("Horário de funcionamento: %s%n", horarioFuncionamento);
 	  }
-
 
 }
