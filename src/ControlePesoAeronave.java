ec811873716e4f88313fafc6a1d3ab4dcd89be70
silva3265 <silva3265@gmail.com>
2024-02-19 17:05:06 -0300

    Estrutura de repetição while

--- src/ControlePesoAeronave.java
+++ src/ControlePesoAeronave.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class ControlePesoAeronave {
-	
+
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 
 		System.out.print("Peso máximo da aeronave: ");
-		int pesoMaximo = entrada.nextInt();
-
-		System.out.print("Quantidade de passageiros: ");
-		int totalPassageiros = entrada.nextInt();
+		int pesoMaximo = entrada.nextInt(); // Usuario Vai digitar o peso maximo
 
 		int pesoTotalPassageiros = 0;
+		boolean incluirNovoPassageiro = true;
 
-		for (int passageiroAtual = 1; passageiroAtual <= totalPassageiros; passageiroAtual++) {
-			System.out.printf("Peso do passageiro #%d: ", passageiroAtual);
+		// while (true) { // o while é usado para repetir uma quantidade indefinida
+		while (pesoTotalPassageiros <= pesoMaximo && incluirNovoPassageiro) {
+			System.out.print("Peso do passageiro: "); 
 			int pesoPassageiro = entrada.nextInt();
 
 			pesoTotalPassageiros += pesoPassageiro;
+
+			System.out.print("Incluir novo passageiro? ");
+			incluirNovoPassageiro = entrada.nextBoolean();
 		}
 
 		System.out.printf("Peso máximo da aeronave: %d kg%n", pesoMaximo);
